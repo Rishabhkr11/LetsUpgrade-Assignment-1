f8332d3a8b8bc12fd5b3115daf4905d88a0c8ade
Rishabh Kumar <69760686+Rishabhkr11@users.noreply.github.com>
2021-03-26 11:31:10 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+var ele = document.getElementsByTagName("h1");
+console.log(ele[0]);
+
+var x = document.getElementsByClassName("example");
+console.log(x);
+
+var test = document.getElementById("imp");
+console.log(test);
